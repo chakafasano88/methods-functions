4e4bac22e424551d0f1efd075da32a534c2d6f54
Matt <matt.fasano88@gmail>
2017-03-22 07:28:08 -0400

    Methods

---
+++ methods.js
@@ -0,0 +1,10 @@
+function Photo(France, Spain, Germany){
+  this.France = France;
+  this.Spain = Spain;
+  this.Germany = Germany;
+}
+
+var myAlbum = new Photo("Croissant", "Paella", "Bratwurst");
+var myAlbum2 = new Photo("Paris", "Barcelona", "Munich");
+
+console.log(myAlbum);
